cfa36056e555e221da5ec4c84a250e008bb7979c
Shaun K <s.ckalagin@gmail.com>
2022-12-09 00:31:16 +0000

    Added hover animation for footer icons. began JS validation

--- Website Pages/script.js
+++ Website Pages/script.js
@@ -22,3 +22,18 @@ function videoUrl(vidInput)
 {
     document.getElementById("slider").src = vidInput
 }
+
+
+
+
+// Email validation
+
+function emailValidation(mail) 
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(bookingform.mail.value))
+  {
+    return (true)
+  }
+    alert("You have entered an invalid email address!")
+    return (false)
+}
